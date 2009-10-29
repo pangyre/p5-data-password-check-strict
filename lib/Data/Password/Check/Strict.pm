7dce0091f20548dbc178079f9922e0be428c5b15
apv <apv@cassidy.local>
2009-10-28 19:18:31 -0700

    Skeleton.

---
+++ lib/Data/Password/Check/Strict.pm
@@ -0,0 +1,75 @@
+package Data::Password::Check::Strict;
+use warnings;
+use strict;
+our $VERSION = "0.01_01";
+
+
+
+1;
+
+__END__
+
+=head1 Name
+
+=head1 Version
+
+0.01_01
+
+=head1 Description
+
+This is an experimental package.
+
+=head1 Synopsis
+
+ use Data::Password::Check::Strict
+
+=head1 To Do
+
+=over 4
+
+=item Everything
+
+=back
+
+=head1 Code Repository
+
+L<http://github.com/pangyre/p5-data-password-check-strict>.
+
+=head1 See Also
+
+L<Data::Password::Check>.
+
+=head1 Author
+
+Ashley Pond V E<middot> ashley.pond.v@gmail.com E<middot> L<http://pangyresoft.com>.
+
+=head1 License
+
+The parts of this library which is Yesh specific is free software. You can redistribute and modify it under the same terms as Perl itself.
+
+=head1 Disclaimer of Warranty
+
+Because this software is licensed free of charge, there is no warranty
+for the software, to the extent permitted by applicable law. Except when
+otherwise stated in writing the copyright holders and other parties
+provide the software "as is" without warranty of any kind, either
+expressed or implied, including, but not limited to, the implied
+warranties of merchantability and fitness for a particular purpose. The
+entire risk as to the quality and performance of the software is with
+you. Should the software prove defective, you assume the cost of all
+necessary servicing, repair, or correction.
+
+In no event unless required by applicable law or agreed to in writing
+will any copyright holder, or any other party who may modify or
+redistribute the software as permitted by the above license, be
+liable to you for damages, including any general, special, incidental,
+or consequential damages arising out of the use or inability to use
+the software (including but not limited to loss of data or data being
+rendered inaccurate or losses sustained by you or third parties or a
+failure of the software to operate with any other software), even if
+such holder or other party has been advised of the possibility of
+such damages.
+
+=cut
+
+
